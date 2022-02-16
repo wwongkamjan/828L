927b8220fbe2347ca4de2c4e0efffea85d2c8fc3
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-16 02:09:32 -0800

    2a

--- layers.py
+++ layers.py
@@ -86,7 +86,7 @@ class Bias:
         # TODO: Compute the result of Bias layer, and store it as self.out_array
         self.out_array = self.in_array + [self.W]*self.in_array.shape[0]
         print("out", self.out_array.shape)
-        print("bias", [self.W]*self.in_array.shape[0].shape)
+        print("bias", ([self.W]*self.in_array.shape[0]).shape)
         return self.out_array
     def backward(self, dwnstrm):
         # TODO: Compute the gradient of the output with respect to W, and store it as G
