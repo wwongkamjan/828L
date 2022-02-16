8ff63897b7f61a7b4ab96289b7322f15a9751369
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-15 23:12:23 -0800

    2a

--- layers.py
+++ layers.py
@@ -42,7 +42,7 @@ class Linear:
         print(dwnstrm.shape)
         
         # TODO: Compute grad of output with respect to inputs, and hand this gradient backward to the layer behind
-        input_grad = np.dot(self.W.T,dwnstrm) # dwnstrm*self.W
+        input_grad = np.dot(dwnstrm, self.W) # dwnstrm*self.W
         # hand this gradient backward to the layer behind
         self.in_layer.backward(input_grad)
 
