4a51c0324c957db010b43b70445df321bfcf25b4
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-15 21:33:40 -0800

    2a

--- layers.py
+++ layers.py
@@ -59,8 +59,8 @@ class Relu:
         out_array = self.out_array.copy()
         out_array[out_array > 0] = 1
         input_grad = out_array*dwnstrm
-        print("d ", dwnstrm.shape)
-        print("in ", self.in_dims)
+        # print("d ", dwnstrm.shape)
+        # print("in ", self.in_dims)
         # hand this gradient backward to the layer behind
         self.in_layer.backward(input_grad)
         pass
