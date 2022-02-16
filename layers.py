e4003b214e0c750cfa8f625e4184f437be51e6b0
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-15 22:55:33 -0800

    2a

--- layers.py
+++ layers.py
@@ -97,6 +97,7 @@ class SquareLoss:
     compute the sum of squares difference between the two"""
     def __init__(self, in_layer, labels):
         self.in_layer = in_layer
+        print(self.in_layer.out_dims)
         self.labels = labels
     def set_data(self, labels):
         self.labels = labels
@@ -111,7 +112,7 @@ class SquareLoss:
         """Gradient is (1/M) (X-Y), where N is the number of training samples"""
         # TODO: Compute grad of output with respect to inputs, and hand this gradient backward to the layer behind
         self.pass_back = (self.in_array - self.labels) *(1/self.num_data)
-        print(self.pass_back)
+
         # hand this gradient backward to the layer behind
         self.in_layer.backward(self.pass_back) 
         pass
