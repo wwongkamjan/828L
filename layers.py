acce7f5200ac3346de53e199c0ec8576f1a83c02
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-15 21:57:43 -0800

    2a

--- layers.py
+++ layers.py
@@ -103,7 +103,7 @@ class SquareLoss:
         self.in_array = self.in_layer.forward()
         self.num_data = self.in_array.shape[0]
         # TODO: Compute the result of mean squared error, and store it as self.out_array
-        self.out_array = np.sum((1/2)*np.abs(self.in_array - self.labels)**2))
+        self.out_array = np.sum((1/2)*np.abs(self.in_array - self.labels)**2)
         return self.out_array
     def backward(self):
         """Gradient is (1/M) (X-Y), where N is the number of training samples"""
