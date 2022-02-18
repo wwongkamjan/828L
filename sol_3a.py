8bb2bee2c0c7b63863f3d3e54394beb5d158de91
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-17 22:25:02 -0800

    update 3

--- sol_3a.py
+++ sol_3a.py
@@ -81,11 +81,8 @@ class Trainer:
     def train(self, num_iter):
         train_losses = []
         #TODO: train the network for num_iter iterations. You should append the loss of each iteration to train_losses.
-        e = 0.001
         for i in range (num_iter):
             train_losses.append(self.train_step())
-            if train_losses[-1] < e:
-                break
         # you have to return train_losses for the function
         print(train_losses[-1])
         return train_losses
