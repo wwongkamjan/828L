b9bba224cb8ab8d9d094c20e43ecadc4bcff3eac
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-17 22:02:27 -0800

    delete print

--- layers.py
+++ layers.py
@@ -69,12 +69,12 @@ class Relu:
         return self.out_array
     def backward(self, dwnstrm):
         # TODO: Compute grad of output with respect to inputs, and hand this gradient backward to the layer behind
-        input_grad = dwnstrm.copy()
-        input_grad = input_grad * self.out_array
-
-        
-        # input_grad[input_grad < 0] = 0
+        input_grad = self.out_array.copy()
         input_grad[input_grad > 0] = 1.
+        input_grad = input_grad * dwnstrm
+
+        # input_grad[input_grad < 0] = 0
+        
         # print("d ", dwnstrm.shape)
         # print("out ", out_array.shape)
         # hand this gradient backward to the layer behind
