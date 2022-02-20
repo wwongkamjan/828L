3766ed2b8f257bebae32f3324a631531585d0ca7
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-19 16:25:14 -0800

    layers update

--- layers.py
+++ layers.py
@@ -71,8 +71,7 @@ class Relu:
         # TODO: Compute grad of output with respect to inputs, and hand this gradient backward to the layer behind
         input_grad = self.out_array.copy()
         input_grad[input_grad > 0] = 1.
-        d = np.repeat(dwnstrm, self.in_dims[0], axis=0)
-        input_grad = input_grad * d
+        input_grad = input_grad * dwnstrm
 
         # input_grad[input_grad < 0] = 0
         
