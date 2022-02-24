fix gradient

sol_mnist.py
@@ -97,7 +97,7 @@ class Trainer:
         correct = 0
         for i in range(len(predict)):
             print("predict: ", np.argmax(predict[i]), " label: ", y[i])
-            print(predict[i])
+            # print(predict[i])
             if np.argmax(predict[i]) == y[i]:
                 correct+=1
         return correct/len(predict)
@@ -140,7 +140,7 @@ def main(test=False):
         # print(train_data.shape)
         train_data_test = (train_x[-1000:],train_y[-1000:])
         test_data = (test_x,test_y)
-        print(trainer.test(train_data_test))
+        # print(trainer.test(train_data_test))
         print(trainer.test(test_data))
 
     else: