fix gradient

layers.py
@@ -215,7 +215,7 @@ class CrossEntropySoftMax:
         # print("one-hot label ",self.ones_hot.shape)
         max_x = np.reshape(np.max(in_array, axis=1), (in_array.shape[0],1))
         # log_exp = max_x + np.log(np.sum(np.exp(in_array - max_x)))
-        exps = np.exp(in_array-max_x)/np.exp(-max_x)
+        exps = np.nan_to_num(np.exp(in_array-max_x)/np.exp(-max_x))
         # softmax = -1*in_array[range(self.num_data),self.labels] + np.log(np.sum(exps))
         softmax = exps/np.sum(exps)
         self.activation = softmax