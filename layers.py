fix gradient

layers.py
@@ -206,8 +206,9 @@ class CrossEntropySoftMax:
         max_xi = np.exp(np.reshape(-np.max(in_array, axis=1), (in_array.shape[0],1)))
         # print(max_xi.shape)
         sum_prob = np.sum(np.exp(in_array)*max_xi/max_xi, axis=1)
-        sum_prob = np.minimum(sum_prob, 708)
-        sum_prob = np.maximum(sum_prob, 1)
+        print(sum_prob)
+        # sum_prob = np.minimum(sum_prob, 708)
+        # sum_prob = np.maximum(sum_prob, 1)
         # print(sum_prob.shape)
         d = [[x]*self.ones_hot.shape[1] for x in sum_prob]
         # self.activation = np.exp(in_array)/d