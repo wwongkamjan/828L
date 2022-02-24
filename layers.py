fix gradient

layers.py
@@ -227,7 +227,7 @@ class CrossEntropySoftMax:
         # print( "activation ", self.activation)
         # log_likelihood = np.nan_to_num(-np.log(softmax[range(self.num_data),self.labels]))
         # self.out_array = -in_array[range(self.num_data),self.labels] + np.log(np.sum(exps))
-        self.out_array = -np.sum(self.ones_hot*np.log(self.activation + 1e-8))
+        self.out_array = -np.mean(self.ones_hot*np.log(self.activation + 1e-8))
         # self.out_array = -np.log(self.activation)
         # print( "loss ", self.out_array)
         # self.out_array= np.nan_to_num(-np.sum(self.ones_hot * log_exp))/self.num_data