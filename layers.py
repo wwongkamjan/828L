3da92454b7b69480d45b1624efb4f3d442762f4f
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-23 16:55:10 -0800

    update mnist test

--- layers.py
+++ layers.py
@@ -204,9 +204,9 @@ class CrossEntropySoftMax:
         in_array = np.minimum(in_array, 708)
         in_array = np.maximum(in_array, -708)
         max_xi = np.exp(np.reshape(-np.max(in_array, axis=1), (in_array.shape[0],1)))
-        print(max_xi.shape)
+        # print(max_xi.shape)
         sum_prob = np.reshape(np.sum(np.exp(in_array)*max_xi/max_xi, axis=1), max_xi.shape)
-        print(sum_prob.shape)
+        # print(sum_prob.shape)
         # d = [[x]*self.ones_hot.shape[1] for x in sum_prob]
         # self.activation = np.exp(in_array)/d
         self.activation = -1*in_array + np.log(sum_prob)
