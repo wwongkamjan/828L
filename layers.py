fix gradient

layers.py
@@ -1,3 +1,4 @@
+from syslog import LOG_LOCAL0
 import numpy as np
 import collections.abc
 
@@ -213,9 +214,13 @@ class CrossEntropySoftMax:
         # print( "activation ", self.activation.shape)
         # print("one-hot label ",self.ones_hot.shape)
         max_x = np.reshape(np.max(in_array, axis=1), (in_array.shape[0],1))
-        log_exp = max_x + np.log(np.sum(np.exp(in_array - max_x)))
-        self.activation = log_exp
-        self.out_array= np.nan_to_num(-np.sum(self.ones_hot * log_exp))/self.num_data
+        # log_exp = max_x + np.log(np.sum(np.exp(in_array - max_x)))
+        exps = np.exp(in_array - max_x)
+        softmax = exps/np.sum(exps)
+        self.activation = softmax
+        log_likelihood = -np.log(softmax[range(self.num_data),self.labels])
+        # self.out_array= np.nan_to_num(-np.sum(self.ones_hot * log_exp))/self.num_data
+        self.out_array= np.nan_to_num(np.sum(log_likelihood))/self.num_data
         return self.out_array
     def backward(self):
         # TODO: Compute grad of loss with respect to inputs, and hand this gradient backward to the layer behind. Be careful! Don't exponentiate an arbitrary positive number as it may overflow. 