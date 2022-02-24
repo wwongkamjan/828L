fix gradient

layers.py
@@ -214,20 +214,20 @@ class CrossEntropySoftMax:
         # self.activation = -1*in_array + np.log(d)
         # print( "activation ", self.activation.shape)
         # print("one-hot label ",self.ones_hot.shape)
-        print("in ",in_array)
+        # print("in ",in_array)
         max_x = np.reshape(np.max(in_array, axis=1), (in_array.shape[0],1))
         # log_exp = max_x + np.log(np.sum(np.exp(in_array - max_x)))
         # exps = np.exp(in_array)
         exps = np.nan_to_num(np.exp(in_array)*np.exp(-max_x)/np.exp(-max_x))
-        print("exps ",exps)
+        # print("exps ",exps)
         # softmax = -1*in_array[range(self.num_data),self.labels] + np.log(np.sum(exps))
         softmax = exps/np.sum(exps)
         self.activation = softmax
-        print( "activation ", self.activation)
+        # print( "activation ", self.activation)
         # log_likelihood = np.nan_to_num(-np.log(softmax[range(self.num_data),self.labels]))
         # self.out_array = -in_array[range(self.num_data),self.labels] + np.log(np.sum(exps))
         self.out_array = -np.log(self.activation)
-        print( "loss ", self.out_array)
+        # print( "loss ", self.out_array)
         # self.out_array= np.nan_to_num(-np.sum(self.ones_hot * log_exp))/self.num_data
         # self.out_array= np.nan_to_num(self.activation)/self.num_data
         return self.out_array