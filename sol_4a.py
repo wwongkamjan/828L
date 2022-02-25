94bee25d15848d03169b961ba5da20454b953400
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-25 01:59:45 -0800

    3b

--- sol_4a.py
+++ sol_4a.py
@@ -112,7 +112,7 @@ def main(test=False):
     #DO NOT REMOVE THESE IF/ELSE
     if not test:
         # Your code goes here.
-        data_dict = data.data_4a_2()
+        data_dict = data.data_4a_3()
         train_data = data_dict['train']
         # print(train_data.shape)
         test_data = data_dict['test']
