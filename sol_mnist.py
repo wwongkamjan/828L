2aa583daf1bf2f3a507bb2fef819ccaabf25e40a
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-24 22:31:10 -0800

    forward

--- sol_mnist.py
+++ sol_mnist.py
@@ -140,7 +140,7 @@ def main(test=False):
                 trainer.loss_layer.set_data(y)
             x,y = train_data
             print("truth: ",y)
-            trainer.train(50)
+            trainer.train(10)
             ind+=batch_size
         
         # print(train_data.shape)
