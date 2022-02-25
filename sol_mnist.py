e0a1ca8f5f44c049a06218d10913dc6152dc7e42
wwongkamjan <w.wongkamjan@gmail.com>
2022-02-24 22:37:05 -0800

    forward

--- sol_mnist.py
+++ sol_mnist.py
@@ -63,7 +63,7 @@ class Trainer:
         #TODO: use the appropriate loss function here
         self.loss_layer = layers.CrossEntropySoftMax(self.network.output_layer, y)
         #TODO: construct the optimizer class here. You can retrieve all modules with parameters (thus need to be optimized be the optimizer) by "network.get_modules_with_parameters()"
-        self.optim = layers.SGDSolver(0.001, self.network.get_modules_with_parameters())
+        self.optim = layers.SGDSolver(0.01, self.network.get_modules_with_parameters())
         return self.data_layer, self.network, self.loss_layer, self.optim
     
     def train_step(self):
@@ -140,7 +140,7 @@ def main(test=False):
                 trainer.loss_layer.set_data(y)
             x,y = train_data
             print("truth: ",y)
-            trainer.train(10)
+            trainer.train(5)
             ind+=batch_size
         
         # print(train_data.shape)
